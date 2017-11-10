0d9312f4d1bf3b12926cc89f93fd3f0461878e56
Etudiant <Etudiant>
2017-11-10 12:34:33 +0100

    Création des fichiers view et public

---
+++ inc/config.dist.php
@@ -0,0 +1,41 @@
+<?php
+
+$config =array(
+    'DB_HOST'=> '',
+    'DB_USER'=> '',
+    'DB_PASSWORD'=> '',
+    'DB_DATABASE'=> '',
+    'MAIL_HOST'=> '',
+    'DB_USERNAME'=> '',
+    'DB_PASSWORD'=> '',
+);
+
+//include de fichiers
+
+require_once __DIR__.'/functions.php';
+
+// Inclusion de composer
+require_once __DIR__.'/../vendor/autoload.php';
+
+// social networks
+//Create a Page instance with the url information
+$socialLinksPage = new SocialLinks\Page([
+    'url' => 'http://catherine_tritz.dev',
+    'title' => 'portfolioCatherine',
+    'text' => 'Extended page description',
+    'image' => 'http://mypage.com/image.png',
+    'icon' => 'http://mypage.com/favicon.png',
+    'twitterUser' => '@twitterUser'
+]);
+
+
+
+
+
+
+
+
+
+
+
+ ?>
---
+++ view/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    Copyright <?php echo date("Y"); ?>
+    <br>
+
+<a href="<?=$socialLinksPage->twitter->shareUrl?>">twitter</a>
+<a href="<?=$socialLinksPage->linkedin->shareUrl?>">linkedin</a>
+</footer>
+</body>
+</html>
